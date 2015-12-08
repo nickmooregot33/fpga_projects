a7930f54d9f82d0a1601a10fddbff5dbf54366e6
Nick Moore <nickmooregot33@gmail.com>
2015-12-08 15:00:40 -0600

    modified loop.c

--- mytests/loop.c
+++ mytests/loop.c
@@ -5,15 +5,16 @@
 int main()
 {
 	int i = 0;
+	char j = 0;
 	volatile unsigned int *LED_PIO = (unsigned int*)0x20; //make a pointer to access the PIO block
 
 	*LED_PIO = 0; //clear all LEDs
 	while ( (1+1) != 3) //infinite loop
 	{
 		for (i = 0; i < 100000; i++); //software delay
-		*LED_PIO |= 0x1; //set LSB
+		*LED_PIO= j++; //set LSB
 		for (i = 0; i < 100000; i++); //software delay
-		*LED_PIO &= ~0x1; //clear LSB
+		*LED_PIO = 0; //clear LSB
 	}
 	return 1; //never gets here
 }
